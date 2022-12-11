4bfb282bd74aae811ff3d93fb5de733e18002313
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2022-12-10 20:41:20 -0600

    added useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import DisplayGif from './components/DisplayGif'
+import Form from './components/Form';
 
 function App() {
+  const [giphyData, setGiphyData] = useState({});
+
+  const makeApiCall = async () => {
+    const giphyUrl = 'https://api.giphy.com/v1/gifs/random?api_key=z39m2fHGZDVYDROVrHuy82c4q8sBDERn&tag=&rating=g';
+   
+    const res = await fetch(giphyUrl);
+    const json = await res.json();
+
+    setGiphyData(json)
+  }
+
+  useEffect(() => {
+    makeApiCall()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
