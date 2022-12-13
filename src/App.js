0d6eead2fa52e7f942c910fe4ac2d5d338165d47
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2022-12-12 19:08:30 -0600

    created 2 components button.js and displaygif.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import { useState, useEffect } from 'react';
+
 import './App.css';
-import DisplayGif from './components/DisplayGif'
-import Button from './components/Button'
+
 
 function App() {
-  const [giphyData, setGiphyData] = useState({});
-
-  const makeApiCall = async () => {
-    const giphyUrl = 'https://api.giphy.com/v1/gifs/random?api_key=z39m2fHGZDVYDROVrHuy82c4q8sBDERn&tag=&rating=g';
-   
-    const res = await fetch(giphyUrl);
-    const json = await res.json();
-
-    setGiphyData(json)
-  }
-
-  useEffect(() => {
-    makeApiCall()
-  }, [])
-
 
 
   return (
     <div className="App">
-      <DisplayGif giphy={giphyData}/>
+
     </div>
   );
 }
