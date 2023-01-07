cc8120aa55aeab99848315224a89a27b5248b1d6
Hanan mohamed <hananmohamed@Hanans-Air.attlocal.net>
2023-01-07 14:40:43 -0600

    removed useeffect

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-Netlify
-https://giphy-hanan.netlify.app/
+Where is your component diagram? Completion looks like:
 
-github
-https://github.com/HananM93/Giphy-lab
+** A diagram of your component hierarchy. ** No gifs will be fetched on intial page load
+
+Netlify https://giphy-hanan.netlify.app/
+
+github https://github.com/HananM93/Giphy-lab
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+
+import { useState } from 'react';
 import './App.css';
 import DisplayGif from './components/DisplayGif'
 import Button from './components/Button'
@@ -12,10 +13,6 @@ function App() {
     const json = await res.json();
     setGiphyData(json)
   }
-
-  useEffect(() => {
-    makeApiCall()
-  }, [])
 
   return (
     <div className="App">
